aa48bf5a153aef3140f4dd1437e2ec84f9534374
pkopyl <petro.kopyl@rsoftstudio.pl>
2020-11-22 12:18:25 +0100

    Request user location

--- src/components/common/app-map/app-map.js
+++ src/components/common/app-map/app-map.js
@@ -2,19 +2,36 @@ import React, { Component } from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import { defaultMapCenter } from "../../../config";
+import * as Location from "expo-location";
 
 export default class AppMap extends Component {
+  state = {
+    userAccessLocation: false,
+  };
+
   mapRef = React.createRef();
 
   animateToRegion(region, duration) {
     this.mapRef.current.animateToRegion(region, duration);
   }
 
+  handleMapReady = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status === "granted") {
+      console.log("granted");
+      this.setState({
+        userAccessLocation: true,
+      });
+    }
+  };
+
   render() {
     const { children } = this.props;
+    const { userAccessLocation } = this.state;
     return (
       <MapView
         ref={this.mapRef}
+        onMapReady={this.handleMapReady}
         initialRegion={{
           latitude: defaultMapCenter.lat,
           longitude: defaultMapCenter.lng,
@@ -23,9 +40,9 @@ export default class AppMap extends Component {
         }}
         showsPointsOfInterest={false}
         showsCompass={false}
-        showsUserLocation
-        showsMyLocationButton
-        style={StyleSheet.absoluteFill}
+        showsUserLocation={userAccessLocation}
+        showsMyLocationButton={userAccessLocation}
+        style={[StyleSheet.absoluteFill]}
       >
         {children}
       </MapView>
