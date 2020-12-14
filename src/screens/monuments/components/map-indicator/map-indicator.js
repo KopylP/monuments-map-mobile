6a7af9d18288f4af081483118e1c1bc1fd7ce066
pkopyl <petro.kopyl@rsoftstudio.pl>
2020-12-14 21:42:24 +0100

    map-indicator refactor

--- src/screens/monuments/components/map-indicator/map-indicator.js
+++ src/screens/monuments/components/map-indicator/map-indicator.js
@@ -1,36 +1,16 @@
 import React from "react";
 import { ActivityIndicator } from "react-native";
+import { StyleSheet } from "react-native";
 import { View } from "react-native";
 import { connect } from "react-redux";
-import AbsoluteIndicator from "../../../../components/template/indicators/absolute-indicator/absolute-indicator";
 import { DefaultTheme } from "../../../../theme/default-theme";
 
 function MapIndicator({ loading }) {
   return (
     <>
       {loading && (
-        <View
-          pointerEvents="none"
-          style={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            right: 0,
-            left: 0,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <View
-            style={{
-              width: 80,
-              height: 80,
-              backgroundColor: "white",
-              borderRadius: 10,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
+        <View pointerEvents="none" style={styles.container}>
+          <View style={styles.indicatorContainer}>
             <ActivityIndicator
               color={DefaultTheme.pallete.colors.primary.main}
               size="large"
@@ -41,6 +21,35 @@ function MapIndicator({ loading }) {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  indicatorContainer: {
+    width: 80,
+    height: 80,
+    backgroundColor: "white",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+});
 
 const bindStateToProps = ({ monuments: { loading } }) => ({ loading });
 
